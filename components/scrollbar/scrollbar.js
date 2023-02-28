3add3ef6822440eee8c698ae53a76b6ee96e76f4
Jakub Kępinski <jakubkepinski87@gmail.com>
2023-02-28 09:25:49 +0100

    scrollbar fix

--- components/scrollbar/scrollbar.js
+++ components/scrollbar/scrollbar.js
@@ -10,35 +10,39 @@ export default function Scrollbar({refScroll, articles}) {
   return (
     <>
         <S.Scrollbar>
-            <S.Container ref={refScroll}>
-                <div className="home">
-                  <span>CAR</span>
-                  <span>Enthusiasts</span>
-                  <span>Forum</span>
-                </div>
 
-                {Object.values(articles).map((article, index) => {
-                  let addon = <Fragment key={(article.carName+index+"fragment").toString()}>
+          <div className="homeContainer">
+            <div className="home">
+                <span>CAR</span>
+                <span>Enthusiasts</span>
+                <span>Forum</span>
+              </div>
+          </div>
+            
+          <S.Container ref={refScroll}>
 
-                    <a href={"/#"+article.carName
-                    .replaceAll(" ", "-")} 
-                    className="segment_main"
-                    onClick={Clicked}
-                    key={(article.carName+index).toString()}
-                    >{article.carName}</a>
+            {Object.values(articles).map((article, index) => {
+              let addon = <Fragment key={(article.carName+index+"fragment").toString()}>
+
+                <a href={"/#"+article.carName
+                .replaceAll(" ", "-")} 
+                className="segment_main"
+                onClick={Clicked}
+                key={(article.carName+index).toString()}
+                >{article.carName}</a>
 
 
-                    {Object.values(article.sections).map((section, index) => {
-                      return <a key={(section.title + index).toString()} href={"/#"+(article.carName+section.title).replaceAll(" ", "")} className="segment_lesser" onClick={Clicked}>{section.title}</a>
-                    })}
-                    <a key={(article.technicalData.title + index).toString()} href={"/#"+(article.carName+article.technicalData.title).replaceAll(" ", "")} className="segment_lesser" onClick={Clicked}>{article.technicalData.title}</a>
-                  
-                  </Fragment>
-
-                  return addon;
-
+                {Object.values(article.sections).map((section, index) => {
+                  return <a key={(section.title + index).toString()} href={"/#"+(article.carName+section.title).replaceAll(" ", "")} className="segment_lesser" onClick={Clicked}>{section.title}</a>
                 })}
-            </S.Container>
+                <a key={(article.technicalData.title + index).toString()} href={"/#"+(article.carName+article.technicalData.title).replaceAll(" ", "")} className="segment_lesser" onClick={Clicked}>{article.technicalData.title}</a>
+              
+              </Fragment>
+
+              return addon;
+
+            })}
+          </S.Container>
         </S.Scrollbar>
     </>
   )
--- components/scrollbar/style.js
+++ components/scrollbar/style.js
@@ -2,8 +2,48 @@ import styled from "styled-components"
 
 export const Scrollbar = styled.div`
 
-    height: 100%;
-    width: 100px;
+    height: 100vh;
+    width: 110px;
+    background-color: #6FC9B9;
+    z-index: 3;
+
+    .homeContainer{
+        width: 110px;
+        z-index: 3;
+        position: fixed; 
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .home{
+        margin-top: 20px;
+        width: fit-content;
+        padding: 10px;
+        box-sizing: border-box;
+        cursor: pointer;
+        background-color: #fff;
+        border-radius: 20px;
+        display: flex;
+        flex-direction: column;
+
+        span{
+            text-align: center;
+            font-weight: 600;
+        }
+
+        span:nth-child(1){
+            font-size: 20px;
+            font-weight: 300;
+        }
+        span:nth-child(2){
+            font-size: 11px;
+        }
+        span:nth-child(3){
+            font-size: 15px;
+        }
+    }
+
 `;
 
 export const Container = styled.div`
@@ -15,9 +55,10 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    gap: 20px;
     background-color: #6FC9B9;
-    z-index: 3;
+    gap: 10px;
+    box-sizing: border-box;
+    padding-top: 100px;
 
     .segment_main{
         width: 50px; 
@@ -74,34 +115,5 @@ export const Container = styled.div`
         height: 20px;
         background-color: grey;
         border-radius: 20px;
-    }
-
-    .home{
-        position: absolute;
-        width: fit-content;
-        padding: 10px;
-        box-sizing: border-box;
-        top: 30px;
-        cursor: pointer;
-        background-color: #fff;
-        border-radius: 20px;
-        display: flex;
-        flex-direction: column;
-
-        span{
-            text-align: center;
-            font-weight: 600;
-        }
-
-        span:nth-child(1){
-            font-size: 20px;
-            font-weight: 300;
-        }
-        span:nth-child(2){
-            font-size: 11px;
-        }
-        span:nth-child(3){
-            font-size: 15px;
-        }
     }
 `;
